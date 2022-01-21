dfdc5a81e788d372397eff9f73e29096a7e811a6
hello tui <33209790+TUISYS@users.noreply.github.com>
2022-01-21 10:49:32 +0800

    Add files via upload

---
+++ apps/page_app8/page_app8.c
@@ -0,0 +1,42 @@
+#include "tui.h"
+
+static tui_obj_t * this_app_obj;
+
+static void page_app8_exit(void);
+tui_obj_t * page_app8_page_view_view_create(void);
+
+static int32_t page_app8_sys_msg_cb(uint32_t cmd, void *param0, void *param1)
+{
+	printf("page_app8 system message:0x%x\n", cmd);
+	switch (cmd)
+	{
+	case TUI_USER_MSG_APP_BACK:
+	case TUI_USER_MSG_APP_HOME:
+		page_app8_exit();
+		tui_sys_msg_send(TUI_USER_MSG_APP_ENTER_HOME, NULL, NULL);
+		break;
+	default:
+		break;
+	}
+	return 0;
+}
+
+tui_obj_t * page_app8_enter(void)
+{
+	tui_sys_msg_reg(page_app8_sys_msg_cb);
+
+	this_app_obj = page_app8_page_view_view_create();
+	//tui_obj_set_click(tui_get_obj_from_id(this_app_obj, PAGE_APP8_PAGE_VIEW_IMAGE_156), 1);
+	//tui_obj_set_click(tui_get_obj_from_id(this_app_obj, PAGE_APP8_PAGE_VIEW_IMAGE_152), 1);
+	//tui_obj_set_click(tui_get_obj_from_id(this_app_obj, PAGE_APP8_PAGE_VIEW_IMAGE_153), 1);
+	//tui_obj_set_click(tui_get_obj_from_id(this_app_obj, PAGE_APP8_PAGE_VIEW_IMAGE_154), 1);
+	//tui_obj_set_click(tui_get_obj_from_id(this_app_obj, PAGE_APP8_PAGE_VIEW_IMAGE_155), 1);
+	tui_obj_set_glue_obj(tui_get_obj_from_id(this_app_obj, PAGE_APP8_PAGE_VIEW_IMAGE_BTN_236), 1);
+	
+	return this_app_obj;
+}
+
+static void page_app8_exit(void)
+{
+	tui_sys_msg_unreg(page_app8_sys_msg_cb);
+}
