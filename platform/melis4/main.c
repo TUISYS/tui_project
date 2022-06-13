5e156df2e6a496a3fd2619a87e473243ede21f6b
hello tui <33209790+TUISYS@users.noreply.github.com>
2022-06-13 16:09:51 +0800

    TUI3.0

--- platform/melis4/main.c
+++ platform/melis4/main.c
@@ -5,5 +5,15 @@ int home_create(void);
 void tui_run(void)
 {
 	home_create();
+	
+	/* kill app_tui self */
+	esEXEC_PDel(EXEC_pidself);
+
+	/* del app_tui main thread */
+	printf("app_tui thread: Bye Bye!\n");
+
+	esKRNL_TDel(OS_PRIO_SELF);
+
+	esKRNL_TimeDly(100);
 }
 #endif
